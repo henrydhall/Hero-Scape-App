7ad616d14026ec255ad77a50ef1353129a8afc0a
Henry Hall <henrydh@byu.edu>
2022-08-14 15:24:54 -0600

    Added functionality to add cards to db from web app

--- python_interface.py
+++ python_interface.py
@@ -26,7 +26,7 @@ def read_cards():
         connection.close()
     return results
 
-def add_card():
+def add_card_console():
     # Get card stats
     card_name = pyinputplus.inputStr('Card name: ')
     card_template = pyinputplus.inputStr('Template: ')
@@ -68,7 +68,30 @@ def add_card():
 def add_many_cards():
     print('Adding many cards.')
     if input('Enter Q now to exit, enter anything else to continue: ') != 'Q':
-        add_card()
+        add_card_console()
+
+def add_card(card_name, card_template, card_life, card_movement, card_range, card_attack, card_defense, \
+        card_points, card_powers, card_species, card_uniqueness, card_squad_number, card_class, \
+            card_personality, card_size_text, card_size_int, card_notes):
+    # We have a ton of inputs, but we just need them.
+    # TODO: verify inputs (either here or in the front end
+    # TODO: consider SQL Injection attack?
+    connection = sqlite3.connect('cards')
+    try:
+        cursor = connection.cursor()
+        # Now we add them via this query string
+        query_string = f'INSERT INTO all_cards (name, template, life, movement, ranged, attack, defense, points, powers, species, uniqueness, squad_number, class, personality, size_text, size_int, notes)    \
+            VALUES    ( \"{card_name}\", \"{card_template}\", {card_life}, {card_movement}, {card_range}, {card_attack}, {card_defense}, {card_points}, \"{card_powers}\", \"{card_species}\", \"{card_uniqueness}\", {card_squad_number}, \"{card_class}\", \"{card_personality}\", \"{card_size_text}\", {card_size_int}, \"{card_notes}\");'
+        print(query_string)
+        cursor.execute(query_string)
+    except sqlite3.Error:
+        connection.rollback()
+        raise
+    else:
+        connection.commit()
+    finally:
+        connection.close()
+    pass
 
 if __name__ == '__main__':
     add_many_cards()
--- tester.py
+++ tester.py
@@ -1,15 +1,45 @@
+# Basic flask libraries
+from crypt import methods
+from pickle import NONE
+from pipes import Template
 from flask import Flask, render_template
+# Bootstrap
 from flask_bootstrap import Bootstrap
+# TODO: figure out what this is
 from flask_moment import Moment
+# Web forms stuff
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
+# Proprietary library
 import python_interface
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard string to guess'
 
 bootstrap = Bootstrap(app)
+
+class Card_Form(FlaskForm):
+    # A form to add cards to the database. 
+    # TODO: validate almost everything.
+    card_name = StringField("Card Name", validators=[DataRequired()])
+    card_template = StringField("Template") 
+    card_life = IntegerField('Life', validators=[DataRequired()])
+    card_movement = IntegerField('Move', validators=[DataRequired()])
+    card_range = IntegerField('Range', validators=[DataRequired()])
+    card_attack = IntegerField('Attack', validators=[DataRequired()])
+    card_defense = IntegerField('Defense', validators=[DataRequired()])
+    card_points = IntegerField('Points', validators=[DataRequired()])
+    card_powers = StringField('Powers/Descriptions')
+    card_species = StringField('Species')
+    card_uniqueness = BooleanField('Unique Hero')
+    card_squad_number = IntegerField('Squad Number')
+    card_class = StringField('Class')
+    card_personality = StringField('Personality')
+    card_size_text = StringField('Size as word')
+    card_size_int = IntegerField('Size as number')
+    card_notes = StringField('Notes')
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -24,3 +54,24 @@ def index():
 def cards():
     cards = python_interface.read_cards()
     return render_template('cards.html', cards=cards)
+
+@app.route('/new_card', methods = ['GET','POST'])
+def add_card():
+    form = Card_Form()
+    card_name = None
+    if form.validate_on_submit():
+        card_name = form.card_name.data
+        form.card_name.data = ''
+        print(card_name)
+        if form.card_uniqueness.data == True:
+            card_uniqueness = 'Hero'
+            card_squad_number = "NULL"
+        else:
+            card_uniqueness = 'Squad'
+            card_squad_number = form.card_squad_number.data
+        python_interface.add_card(card_name, form.card_template.data, form.card_life.data, \
+            form.card_movement.data, form.card_range.data, form.card_attack.data, form.card_defense.data, \
+            form.card_points.data, form.card_powers.data, form.card_species.data, card_uniqueness, \
+            card_squad_number, form.card_class.data, form.card_personality.data, \
+            form.card_size_text.data, form.card_size_int.data, form.card_notes.data)
+    return render_template('new_card.html', form = form, card_name = card_name)
